631a8f80f5d7acdbb5a1b8c19011b1d5092d2373
DongjunLee <humanbrain.djlee@gmail.com>
2017-12-10 05:00:09 +0900

    feat(data_loader): add get_max_seq_length and pad_input

--- data_loader.py
+++ data_loader.py
@@ -11,20 +11,27 @@ from tqdm import tqdm
 
 class DataLoader:
 
-    def __init__(self, task_id, task_test_id, w2v_dim=100, input_mask_mode="sentence"):
+    def __init__(self, task_id, task_test_id, w2v_dim=100, input_mask_mode="sentence", use_pretrained=True):
         self.base_path = os.path.join("data/")
 
         self.task_id = str(task_id)
         self.task_test_id = str(task_test_id)
         self.w2v_dim = w2v_dim
         self.input_mask_mode = input_mask_mode
+        self.use_pretrained = use_pretrained
 
-    def make_train_ans_test_set(self):
+    def make_train_and_test_set(self):
         train_raw, test_raw = self.get_babi_raw(self.task_id, self.task_test_id)
+        self.max_facts_seq_len, self.max_question_seq_len, self.max_input_mask_len = self.get_max_seq_length(train_raw, test_raw)
 
-        self.word2vec = self.load_glove(self.w2v_dim)
+        if self.use_pretrained:
+            self.word2vec = self.load_glove(self.w2v_dim)
+        else:
+            self.word2vec = {}
         self.vocab = {}
         self.ivocab = {}
+
+        self.create_vector("unknown")
 
         train_input, train_question, train_answer, train_input_mask = self.process_input(train_raw)
         test_input, test_question, test_answer, test_input_mask = self.process_input(test_raw)
@@ -34,6 +41,18 @@ class DataLoader:
             "test": (test_input, test_input_mask, test_question, test_answer)
         }
 
+    def get_max_seq_length(self, *datasets):
+        max_facts_length, max_question_length, max_input_mask_length = 0, 0, 0
+
+        def count_punctuation(facts):
+            return len(list(filter(lambda x: x == ".", facts)))
+
+        for dataset in datasets:
+            for d in dataset:
+                max_facts_length = max(max_facts_length, len(d['C'].split()))
+                max_input_mask_length = max(max_input_mask_length, count_punctuation(d['C']))
+                max_question_length = max(max_question_length, len(d['Q'].split()))
+        return max_facts_length, max_question_length, max_input_mask_length
 
     def init_babi(self, fname):
         print("==> Loading test from %s" % fname)
@@ -109,10 +128,9 @@ class DataLoader:
             test_id = id
         babi_name = babi_map[id]
         babi_test_name = babi_map[test_id]
-        babi_train_raw = self.init_babi(os.path.join(self.base_path, 'en-10k/%s_train.txt' % babi_name))
-        babi_test_raw = self.init_babi(os.path.join(self.base_path, 'en-10k/%s_test.txt' % babi_test_name))
+        babi_train_raw = self.init_babi(os.path.join(self.base_path, 'en-10/%s_train.txt' % babi_name))
+        babi_test_raw = self.init_babi(os.path.join(self.base_path, 'en-10/%s_test.txt' % babi_test_name))
         return babi_train_raw, babi_test_raw
-
 
     def load_glove(self, dim):
         word2vec = {}
@@ -127,19 +145,17 @@ class DataLoader:
 
         return word2vec
 
-
     def create_vector(self, word, silent=False):
         # if the word is missing from Glove, create some fake vector and store in glove!
-        vector = np.random.uniform(0.0,1.0,(self.w2v_dim,))
+        vector = np.random.uniform(0.0, 1.0, (self.w2v_dim,))
         self.word2vec[word] = vector
         if (not silent):
             print("data_loader.py::create_vector => %s is missing" % word)
         return vector
 
-
     def process_word(self, word, to_return="word2vec", silent=False):
         if not word in self.word2vec:
-            self.create_vector(word, self.word2vec, self.w2v_dim, silent)
+            self.create_vector(word, silent=silent)
         if not word in self.vocab:
             next_index = len(self.vocab)
             self.vocab[word] = next_index
@@ -152,7 +168,6 @@ class DataLoader:
         else:
             raise ValueError("return type is 'word2vec' or 'index'")
 
-
     def get_norm(self, x):
         x = np.array(x)
         return np.sum(x * x)
@@ -162,27 +177,37 @@ class DataLoader:
         inputs = []
         answers = []
         input_masks = []
+
         for x in data_raw:
             inp = x["C"].lower().split(' ')
             inp = [w for w in inp if len(w) > 0]
+
             q = x["Q"].lower().split(' ')
             q = [w for w in q if len(w) > 0]
 
             inp_vector = [self.process_word(word=w, to_return="word2vec") for w in inp]
-            q_vector = [self.process_word(word=w, to_return="word2vec") for w in q]
+            inp_vector = self.pad_input(inp_vector, self.max_facts_seq_len, [np.zeros(self.w2v_dim)])
 
+            q_vector = [self.process_word(word=w, to_return="word2vec") for w in q]
+            q_vector = self.pad_input(q_vector, self.max_question_seq_len, [np.zeros(self.w2v_dim)])
 
             inputs.append(np.vstack(inp_vector).astype(float))
             questions.append(np.vstack(q_vector).astype(float))
             answers.append(self.process_word(word = x["A"], to_return = "index"))
 
-            # NOTE: here we assume the answer is one word!
             if self.input_mask_mode == 'word':
                 input_masks.append(np.array([index for index, w in enumerate(inp)], dtype=np.int32))
             elif self.input_mask_mode == 'sentence':
-                input_masks.append(np.array([index for index, w in enumerate(inp) if w == '.'], dtype=np.int32))
+                input_mask = [index for index, w in enumerate(inp) if w == '.']
+                input_mask = self.pad_input(input_mask, self.max_input_mask_len, [0])
+                input_masks.append(input_mask)
             else:
                 raise ValueError("input_mask_mode is only available (word, sentence)")
 
-        return inputs, questions, answers, input_masks
+        return (np.array(inputs, dtype=np.float32),
+                np.array(questions, dtype=np.float32),
+                np.array(answers, dtype=np.int32).reshape(-1, 1),
+                np.array(input_masks, dtype=np.int32))
 
+    def pad_input(self, input_, size, pad_item):
+        return input_ + pad_item * (size - len(input_))
