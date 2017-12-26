9f4a829b909d5c4c91b7484e3374ae9d3f03908b
DongjunLee <humanbrain.djlee@gmail.com>
2017-12-26 18:10:10 +0900

    refactor: migrate dataset to data_loader's make_batch, experiment to main's experiment_fn

--- data_loader.py
+++ data_loader.py
@@ -3,8 +3,11 @@ bAbi data_loader
 Original code : https://github.com/YerevaNN/Dynamic-memory-networks-in-Theano/blob/master/utils.py
 """
 
-import os as os
+import os
+
+from hbconfig import Config
 import numpy as np
+import tensorflow as tf
 from tqdm import tqdm
 
 
@@ -212,3 +215,68 @@ class DataLoader:
 
     def pad_input(self, input_, size, pad_item):
         return input_ + pad_item * (size - len(input_))
+
+    def make_batch(self, data, buffer_size=10000, batch_size=64, scope="train"):
+
+        class IteratorInitializerHook(tf.train.SessionRunHook):
+            """Hook to initialise data iterator after Session is created."""
+
+            def __init__(self):
+                super(IteratorInitializerHook, self).__init__()
+                self.iterator_initializer_func = None
+
+            def after_create_session(self, session, coord):
+                """Initialise the iterator after the session has been created."""
+                self.iterator_initializer_func(session)
+
+
+        iterator_initializer_hook = IteratorInitializerHook()
+
+        def get_inputs():
+            with tf.name_scope(scope):
+
+                inputs, input_masks, questions, answers = data
+
+                # Define placeholders
+                input_placeholder = tf.placeholder(
+                    tf.float32, [None, Config.data.max_facts_seq_len, Config.model.embed_dim])
+                input_mask_placeholder = tf.placeholder(
+                    tf.int32, [None, Config.data.max_input_mask_length])
+                question_placeholder = tf.placeholder(
+                    tf.float32, [None, Config.data.max_question_seq_len, Config.model.embed_dim])
+                answer_placeholder = tf.placeholder(
+                    tf.int32, [None, 1])
+
+                # Build dataset iterator
+                dataset = tf.data.Dataset.from_tensor_slices(
+                    (input_placeholder, input_mask_placeholder,
+                    question_placeholder, answer_placeholder))
+
+                if scope == "train":
+                    dataset = dataset.repeat(None)  # Infinite iterations
+                else:
+                    dataset = dataset.repeat(1)  # one Epoch
+
+                dataset = dataset.shuffle(buffer_size=buffer_size)
+                dataset = dataset.batch(batch_size)
+
+                iterator = dataset.make_initializable_iterator()
+                next_input, next_input_mask, next_question, next_answer = iterator.get_next()
+
+                # Set runhook to initialize iterator
+                iterator_initializer_hook.iterator_initializer_func = \
+                    lambda sess: sess.run(
+                        iterator.initializer,
+                        feed_dict={input_placeholder: inputs,
+                                input_mask_placeholder: input_masks,
+                                question_placeholder: questions,
+                                answer_placeholder: answers})
+
+                # Return batched (features, labels)
+                features = {"input_data": next_input,
+                            "input_data_mask": next_input_mask,
+                            "question_data": next_question}
+                return (features, next_answer)
+
+        # Return function and hook
+        return get_inputs, iterator_initializer_hook
--- main.py
+++ main.py
@@ -5,9 +5,67 @@ import logging
 
 from hbconfig import Config
 import tensorflow as tf
+from tensorflow.python import debug as tf_debug
 
-import experiment
+from data_loader import DataLoader
+import hook
+from model import DMN
 
+
+def experiment_fn(run_config, params):
+
+    dmn_model = DMN()
+    estimator = tf.estimator.Estimator(
+            model_fn=dmn_model.model_fn,
+            model_dir=Config.train.model_dir,
+            params=params,
+            config=run_config)
+
+    data_loader = DataLoader(
+            task_path=Config.data.task_path,
+            task_id=Config.data.task_id,
+            task_test_id=Config.data.task_id,
+            w2v_dim=Config.model.embed_dim,
+            use_pretrained=Config.model.use_pretrained)
+
+    data = data_loader.make_train_and_test_set()
+
+    vocab = data_loader.vocab
+
+    # setting data property
+    Config.data.vocab_size = len(vocab)
+    Config.data.max_facts_seq_len = data_loader.max_facts_seq_len
+    Config.data.max_question_seq_len = data_loader.max_question_seq_len
+    Config.data.max_input_mask_length = data_loader.max_input_mask_len
+    print("max_facts_seq_len:", data_loader.max_facts_seq_len)
+    print("max_question_seq_len:", data_loader.max_question_seq_len)
+    print("max_input_mask_length:", data_loader.max_input_mask_len)
+
+    train_input_fn, train_input_hook = data_loader.make_batch(
+            data["train"], batch_size=Config.model.batch_size, scope="train")
+    test_input_fn, test_input_hook = data_loader.make_batch(
+            data["test"], batch_size=Config.model.batch_size, scope="test")
+
+    train_hooks = [train_input_hook]
+    if Config.train.print_verbose:
+        pass
+    if Config.train.debug:
+        train_hooks.append(tf_debug.LocalCLIDebugHook())
+
+    eval_hooks = [test_input_hook]
+    if Config.train.debug:
+        eval_hooks.append(tf_debug.LocalCLIDebugHook())
+
+    experiment = tf.contrib.learn.Experiment(
+        estimator=estimator,
+        train_input_fn=train_input_fn,
+        eval_input_fn=test_input_fn,
+        train_steps=Config.train.train_steps,
+        min_eval_frequency=Config.train.min_eval_frequency,
+        train_monitors=train_hooks,
+        eval_hooks=eval_hooks
+    )
+    return experiment
 
 
 def main(mode):
@@ -18,7 +76,7 @@ def main(mode):
             save_checkpoints_steps=Config.train.save_checkpoints_steps)
 
     tf.contrib.learn.learn_runner.run(
-        experiment_fn=experiment.experiment_fn,
+        experiment_fn=experiment_fn,
         run_config=run_config,
         schedule=mode,
         hparams=params
@@ -39,5 +97,9 @@ if __name__ == '__main__':
 
     Config(args.config)
     print("Config: ", Config)
+    if Config.description:
+        print("Config Description")
+        for key, value in Config.description.items():
+            print(f" - {key}: {value}")
 
     main(args.mode)
